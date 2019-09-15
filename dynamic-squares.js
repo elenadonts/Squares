b4495c182264c52bd7ff4735b4cfe047924eb989
Elena Dontsova <elenadonts@gmail.com>
2019-09-15 23:36:35 +0300

    refactor constructor and fix elements layout

--- dynamic-squares.js
+++ dynamic-squares.js
@@ -1,11 +1,8 @@
 "use strict";
 
-let currentTableWidth = 1;
-let currentTableHeight = 1;
-
 const cellBorderSize = 1;
 const cellSize = 70;
-const initialLeftMargin = cellSize + cellBorderSize * 3;
+const initialLeftMargin = cellSize + cellBorderSize * 2;
 
 const tagName = "dynamic-squares";
 const template = document.querySelector("template");
@@ -14,6 +11,8 @@ customElements.define(tagName,
     class DynamicSquares extends HTMLElement {
         constructor(){
             super();
+            let currentTableWidth = 1;
+            let currentTableHeight = 1;
             const shadowRoot = this.attachShadow({mode: 'open'});
             shadowRoot.appendChild(template.content.cloneNode(true));
 
@@ -23,38 +22,26 @@ customElements.define(tagName,
             const rightAddButton = shadowRoot.querySelector("#right-add-button");
             const bottomAddButton = shadowRoot.querySelector("#bottom-add-button");
 
-            //move delete buttons according to the mouse position
-            squaresTable.addEventListener("mouseover", function (mousePosition) {
-                let currentColIndex = mousePosition.path[0].cellIndex;
-                let currentRowIndex = mousePosition.path[1].rowIndex;
-                let nextTopDeleteButtonPosition = currentColIndex * (cellSize + cellBorderSize * 2) + initialLeftMargin;
-                let nextLeftDeleteButtonPosition = currentRowIndex * (cellSize + cellBorderSize * 2);
-                topDeleteButton.style.marginLeft = nextTopDeleteButtonPosition + "px";
-                leftDeleteButton.style.marginTop = nextLeftDeleteButtonPosition + "px";
-            });
-
-            // hide delete buttons onmouseleave from elements below
-            [squaresTable, topDeleteButton, leftDeleteButton].forEach(function (element) {
-                element.addEventListener("mouseleave",
-                    () => hideDeleteButtons(false));
-            });
-
-            // show "delete row" button if there's more than 1 row and "delete column" if there's more than 1 column
-            [squaresTable, topDeleteButton, leftDeleteButton].forEach(function (element) {
-                element.addEventListener("mouseover", showDeleteButtons);
-            });
-
             rightAddButton.addEventListener("click", addColumn);
             bottomAddButton.addEventListener("click", addRow);
             leftDeleteButton.addEventListener("click", deleteRow);
             topDeleteButton.addEventListener("click", deleteColumn);
 
+            // show "delete row" button if there's more than 1 row and "delete column" if there's more than 1 column
+            // hide delete buttons onmouseleave from elements below
+            [squaresTable, topDeleteButton, leftDeleteButton].forEach(function (element) {
+                element.addEventListener("mouseleave", hideDeleteButtons.bind(this, false));
+                element.addEventListener("mouseover", showDeleteButtons);
+            });
+
+            //move delete buttons according to the mouse position
+            squaresTable.addEventListener("mouseover", moveDeleteButtons);
+
             function addColumn() {
-                for (let i = 0; i < currentTableHeight; i++){
-                    let currentRow = squaresTable.rows[i];
-                    let currentRowLastCell = squaresTable.rows[i].cells[currentTableWidth - 1];
+                for (let row of squaresTable.rows){
+                    let currentRowLastCell = row.cells[currentTableWidth - 1];
                     let clone = currentRowLastCell.cloneNode(true);
-                    currentRow.appendChild(clone);
+                    row.appendChild(clone);
                 }
                 currentTableWidth++;
             }
@@ -74,27 +61,37 @@ customElements.define(tagName,
             }
 
             function deleteColumn(){
-                for (let i = 0; i < currentTableHeight; i++){
-                    let currentRow = squaresTable.rows[i];
-                    currentRow.deleteCell(-1);
+                if (currentTableWidth === 1) return;
+                for (let row of squaresTable.rows){
+                    row.deleteCell(-1);
                 }
                 hideDeleteButtons(true);
                 currentTableWidth--;
             }
 
+            function moveDeleteButtons(event){
+                let currentColIndex = event.path[0].cellIndex;
+                let currentRowIndex = event.path[1].rowIndex;
+                let nextTopDeleteButtonPosition = currentColIndex * (cellSize + cellBorderSize * 2) + initialLeftMargin;
+                let nextLeftDeleteButtonPosition = currentRowIndex * (cellSize + cellBorderSize * 2);
+                topDeleteButton.style.marginLeft = nextTopDeleteButtonPosition + "px";
+                leftDeleteButton.style.marginTop = nextLeftDeleteButtonPosition + "px";
+            }
+
             function hideDeleteButtons(elementWasDeleted) {
-                if (!elementWasDeleted && (leftDeleteButton.matches(":hover") || topDeleteButton.matches(":hover")))
+                if (!elementWasDeleted && (leftDeleteButton.matches(":hover") ||
+                    topDeleteButton.matches(":hover")))
                     return;
 
-                topDeleteButton.style.visibility = "hidden";
-                leftDeleteButton.style.visibility = "hidden";
+                topDeleteButton.style.display = "none";
+                leftDeleteButton.style.display = "none";
             }
 
             function showDeleteButtons(){
-                let leftButtonVisibility = currentTableWidth > 1 ? "visible" : "hidden";
-                let topButtonVisibility = currentTableHeight > 1 ? "visible" : "hidden";
-                leftDeleteButton.style.visibility = leftButtonVisibility;
-                topDeleteButton.style.visibility = topButtonVisibility;
+                let leftButtonVisibility = currentTableHeight > 1 ? "block" : "none";
+                let topButtonVisibility = currentTableWidth > 1 ? "block" : "none";
+                leftDeleteButton.style.display = leftButtonVisibility;
+                topDeleteButton.style.display = topButtonVisibility;
             }
         }
     });
